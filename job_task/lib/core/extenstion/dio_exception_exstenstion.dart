bdce957006f6a18159c385af03234a2d32a43d75
rubahammad96icloud.com <rubaahmedhsmmsd@gmail.com>
2026-07-11 02:33:31 +0300

    carts connection

--- job_task/lib/core/extenstion/dio_exception_exstenstion.dart
+++ job_task/lib/core/extenstion/dio_exception_exstenstion.dart
@@ -1,23 +1,1 @@
-import 'package:dio/dio.dart';
-
-class DioExceptionExtension {
-  static String parseDioError(DioException e) {
-    final data = e.response?.data;
-
-    if (data is Map<String, dynamic>) {
-      return data['message']?.toString() ?? 'Server Error';
-    }
-
-    return switch (e.type) {
-      DioExceptionType.connectionError => 'No Internet Connection',
-      DioExceptionType.connectionTimeout => 'Connection Timeout',
-      DioExceptionType.receiveTimeout => 'Receive Timeout',
-      DioExceptionType.sendTimeout => 'Send Timeout',
-      DioExceptionType.badResponse => 'Server Error',
-      DioExceptionType.badCertificate => 'Bad Certificate',
-      DioExceptionType.cancel => 'Request Cancelled',
-      DioExceptionType.unknown => e.message ?? 'Unknown Error',
-      _ => e.message ?? 'Unknown Error',
-    };
-  }
-}
+import 'package:dio/dio.dart';class DioExceptionExtension {  static String parseDioError(DioException e) {    final data = e.response?.data;    if (data is Map<String, dynamic>) {      return data['message']?.toString() ?? 'Server Error';    }    return switch (e.type) {      DioExceptionType.connectionError => 'No Internet Connection',      DioExceptionType.connectionTimeout => 'Connection Timeout',      DioExceptionType.receiveTimeout => 'Receive Timeout',      DioExceptionType.sendTimeout => 'Send Timeout',      DioExceptionType.badResponse => 'Server Error',      DioExceptionType.badCertificate => 'Bad Certificate',      DioExceptionType.cancel => 'Request Cancelled',      DioExceptionType.unknown => e.message ?? 'Unknown Error',      _ => e.message ?? 'Unknown Error',    };  }}
