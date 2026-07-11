d868d9b54f11b4d0559df196caf5c5ac6355dd3b
rubahammad96icloud.com <rubaahmedhsmmsd@gmail.com>
2026-07-11 16:05:46 +0300

    faviorates and rename same files connection

--- job_task/lib/domain/use_cases/faviorate/get_fav_use_case.dart
+++ job_task/lib/domain/use_cases/faviorate/get_fav_use_case.dart
@@ -1,9 +1,8 @@
 
 import 'package:injectable/injectable.dart';
 import 'package:job_task/core/di/api_result.dart';
+import 'package:job_task/data/model/response/favorite_entity.dart';
 
-import 'package:job_task/data/model/response/cart_entity.dart';
-import 'package:job_task/data/model/response/faviorate_entity.dart';
 import 'package:job_task/domain/repository/home_page_repo.dart';
 
 @singleton
